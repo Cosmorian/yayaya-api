1697eb13a65eb42c3ed600309825cfb14cbe26ff
codemilli <hckrmoon@gmail.com>
2019-07-11 13:58:37 +0900

    wip

--- api/handlers/games/index.ts
+++ api/handlers/games/index.ts
@@ -2,28 +2,53 @@ import { getApp } from "../../app"
 import DynamoEntity from "../../entity/DynamoEntity"
 const app = getApp();
 
+function createNArray(n = 0) {
+  return [...Array(n).keys()];
+}
+
 app.get('*', async (req, res) => {
-  let { games = '' } = req.query;
-  if (!games) {
-    return res.status(400).json('Bad Request');
+  const { nowS, roundedNow } = getRoundedNow();
+  const list = createNArray(20).reduce((acc, item) => {
+    acc.arr[item] = (acc.roundedNow - (60000 * item));
+    return acc;
+  }, {
+    arr: [],
+    roundedNow,
+  }).arr.reverse();
+  let gameState = 'ready';
+  if (nowS >= 20) {
+    gameState = nowS < 40 ? 'onprogress' : 'done';
   }
-  games = games
-    .split(',')
-    .filter(g => g)
-    .map(g => Number(g));
-  let results = await getGames(games);
+  let results = await getGames(list);
   const existsIds = results.map(ya => (ya && ya.gameId)).filter(i => i);
-  const promises = games.map((game) => {
+  const promises = list.map((game) => {
     if (existsIds.indexOf(game) === -1) {
       return DynamoEntity.putItem(game);
     }
   }).filter(r => r);
   if (promises.length) {
     await Promise.all(promises);
-    results = await getGames(games);
+    results = await getGames(list);
   }
-  res.json({ data: results });
+  res.json({
+    data: {
+      results,
+      gameState,
+    },
+  });
 });
+
+function getRoundedNow() {
+  const now = new Date();
+  const nowTs = now.valueOf();
+  const nowMs = now.getUTCMilliseconds();
+  const nowS = now.getUTCSeconds() * 1000;
+  const roundedNow = nowTs - nowMs - nowS;
+  return {
+    roundedNow,
+    nowS,
+  };
+}
 
 async function getGames(games: number[]) {
   const { Responses: { yayaya = [] } } = await DynamoEntity.queryIds(games);
